b816475a5c7ae9e96eeca326bedacaf8be137e08
Arbaaz Ahmad <ArbaazA@bpl.netlink.com>
2022-06-13 18:22:15 +0530

    commited

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {useEffect, useState} from 'react'
 function App() {
+  const [date,setDate]=useState()
+  const [seconds,setSeconds]=useState()
+  //{hour:'',minutes:'' ,seconds:''}
+  setInterval(()=>{
+    const date=new Date()
+    const h=date.getHours()
+    const m=date.getMinutes()
+    const s=date.getSeconds()
+    setDate(h)
+    setSeconds(s)
+},1000)
+
+  useEffect(()=>{
+
+ },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App">     
+<div className='clock'>
+         <p className='no1'>1</p>
+         <p className='no2'>2</p>
+         <p className='no3'>3</p>
+         <p className='no4'>4</p>
+         <p className='no5'>5</p>
+         <p className='no6'>6</p>
+         <p className='no7'>7</p>
+         <p className='no8'>8</p>
+         <p className='no9'>9</p>
+         <p className='no10'>10</p>
+         <p className='no11'>11</p>
+         <p className='no12'>12</p>
+      <div className='hour'></div>
+     <div className='minute'></div>
+      <div className='seconds'></div>
+        </div><br></br>
+     
     </div>
   );
 }
